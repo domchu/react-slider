dbe024336162f141977164e0b3e79332bee56ad0
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-08 20:51:56 +0100

    chore: rendering images as json

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,5 +1,29 @@
 export const Data = [
-  //   {
-  //     image: "../../public/images/slider1",
-  //   },
+  {
+    image: "../../public/images/slider1",
+  },
+  {
+    image: "../../public/images/slider2",
+  },
+  {
+    image: "../../public/images/slider3",
+  },
+  {
+    image: "../../public/images/slider4",
+  },
+  {
+    image: "../../public/images/slider5",
+  },
+  {
+    image: "../../public/images/slider6",
+  },
+  {
+    image: "../../public/images/slider7",
+  },
+  {
+    image: "../../public/images/slider8",
+  },
+  {
+    image: "../../public/images/slider9",
+  },
 ];
