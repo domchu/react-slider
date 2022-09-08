4019b34f6b737f33d35f90de136ea3a01a4b3db1
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-08 23:05:39 +0100

    chore:importing data to the imageslider

--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Data } from "./Data";
 
 const ImageSlider = () => {
   return (
     <>
       {Data.map((slide, index) => {
-        return <img src={slide.image} alt={travel} />;
+        return <img src={slide.image} alt="travel" />;
       })}
     </>
   );
