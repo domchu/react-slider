3d3eb615ccfb8f301e04373cb602309bc419c2a0
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-09 19:16:47 +0100

    chore:passing props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
+import Data from "./Components/Data";
 import ImageSlider from "./Components/ImageSlider";
 
 function App() {
-  return <ImageSlider />;
+  return <ImageSlider slides={Data} />;
 }
 
 export default App;
--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -3,14 +3,14 @@ import Data from "./Data";
 import { BsFillArrowRightSquareFill } from "react-icons/bs";
 import { BsFillArrowLeftSquareFill } from "react-icons/bs";
 
-const ImageSlider = () => {
+const ImageSlider = (slides) => {
   const [current, setCount] = useState(0);
-  // const length = slides.length;
+  const length = slides.length;
 
   return (
     <section className="slider">
-      <BsFillArrowLeftSquareFill className="left-arrow" onClick={prev} />
-      <BsFillArrowRightSquareFill className="right-arrow" onClick={next} />
+      <BsFillArrowLeftSquareFill className="left-arrow" />
+      <BsFillArrowRightSquareFill className="right-arrow" />
       {Data.map((slide, index) => {
         return (
           <img src={slide} alt="travel" key={index} className="slider-image" />
