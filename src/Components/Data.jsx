e0904964428b33cbd1bd92bbaaead9506f2355b0
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-09 11:23:06 +0100

    chore:imported all the images and export it

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,36 +1,23 @@
 import slider1 from "./images/slider1.jpg";
 import slider2 from "./images/slider2.jpg";
+import slider3 from "./images/slider3.jpg";
+import slider4 from "./images/slider4.jpg";
+import slider5 from "./images/slider5.jpg";
+import slider6 from "./images/slider6.jpg";
+import slider7 from "./images/slider7.jpg";
+import slider8 from "./images/slider8.jpg";
+import slider9 from "./images/slider9.jpg";
 
 const Data = [
   slider1,
   slider2,
-  // {
-  //   image: "./images/slider1.jpg",
-  // },
-  // {
-  //   image: "./images/slider2.jpg",
-  // },
-  // {
-  //   image: "./images/slider3.jpg",
-  // },
-  // {
-  //   image: "./images/slider4.jpg",
-  // },
-  // {
-  //   image: "./images/slider5.jpg",
-  // },
-  // {
-  //   image: "./images/slider6.jpg",
-  // },
-  // {
-  //   image: "./images/slider7.jpg",
-  // },
-  // {
-  //   image: "./images/slider8.jpg",
-  // },
-  // {
-  //   image: "./images/slider9.jpg",
-  // },
+  slider3,
+  slider4,
+  slider5,
+  slider6,
+  slider7,
+  slider8,
+  slider9,
 ];
 
 export default Data;
