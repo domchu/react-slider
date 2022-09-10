d4b42777eddcfd0de232f088a711d3f8c1c9ac73
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-11 00:50:03 +0100

    feature:create json file form images

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,23 +1,32 @@
-import slider1 from "./images/slider1.jpg";
-import slider2 from "./images/slider2.jpg";
-import slider3 from "./images/slider3.jpg";
-import slider4 from "./images/slider4.jpg";
-import slider5 from "./images/slider5.jpg";
-import slider6 from "./images/slider6.jpg";
-import slider7 from "./images/slider7.jpg";
-import slider8 from "./images/slider8.jpg";
-import slider9 from "./images/slider9.jpg";
+import Assets from "../Components/Constants/Assets";
 
-const Data = [
-  slider1,
-  slider2,
-  slider3,
-  slider4,
-  slider5,
-  slider6,
-  slider7,
-  slider8,
-  slider9,
+const Date = [
+  {
+    images: "",
+  },
 ];
-
 export default Data;
+
+// import slider1 from "./images/slider1.jpg";
+// import slider2 from "./images/slider2.jpg";
+// import slider3 from "./images/slider3.jpg";
+// import slider4 from "./images/slider4.jpg";
+// import slider5 from "./images/slider5.jpg";
+// import slider6 from "./images/slider6.jpg";
+// import slider7 from "./images/slider7.jpg";
+// import slider8 from "./images/slider8.jpg";
+// import slider9 from "./images/slider9.jpg";
+
+// const Data = [
+//   slider1,
+//   slider2,
+//   slider3,
+//   slider4,
+//   slider5,
+//   slider6,
+//   slider7,
+//   slider8,
+//   slider9,
+// ];
+
+// export default Data;
